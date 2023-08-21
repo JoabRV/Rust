5d69e6c0f7f4ad0d937d0b56cfce4ec55ccf6799
Joab Ramos <joab.ramos27@gmail.com>
2023-08-20 19:38:10 -0600

    my first HelloWorld

---
+++ hola_mundo/target/debug/deps/hola_mundo-c387a103569b3503.d
@@ -0,0 +1,5 @@
+/home/joab/git/Rust/hola_mundo/target/debug/deps/hola_mundo-c387a103569b3503: src/main.rs
+
+/home/joab/git/Rust/hola_mundo/target/debug/deps/hola_mundo-c387a103569b3503.d: src/main.rs
+
+src/main.rs:
---
+++ hola_mundo/target/debug/hola_mundo.d
@@ -0,0 +1,1 @@
+/home/joab/git/Rust/hola_mundo/target/debug/hola_mundo: /home/joab/git/Rust/hola_mundo/src/main.rs
